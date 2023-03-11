25591e04a76a2885b070bb8f2c5f6d46c9b9e63d
piedrahitapablo <piedra@meltstudio.co>
2023-03-11 07:48:00 -0500

    add settings for folding and splits

--- lua/piedra/set.lua
+++ lua/piedra/set.lua
@@ -1,5 +1,6 @@
+-- line numbers
 vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 
 -- show indentation and blank lines
 vim.opt.list = true
@@ -7,21 +8,43 @@ vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "trail:⋅"
 -- vim.opt.listchars:append "eol:↴"
 
+-- tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- text wrapping
 vim.opt.wrap = false
+
+-- code folding
+vim.opt.foldcolumn = '1'
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = -1
+vim.opt.foldenable = true
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- title
+vim.opt.title = true
+-- uncomment to set a custom title, this is not the default but it's very
+-- similar
+-- https://github.com/neovim/neovim/issues/1248#issuecomment-1185487263
+-- vim.opt.titlestring = '%t%(\ %M%)%(\ \(%{expand(\"%:~:h\")}\)%)%a\ -\ NVIM'
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
--- vim.opt.hlsearch = true
+-- search
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
+
+-- splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 vim.opt.termguicolors = true
 
