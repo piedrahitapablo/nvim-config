5c0af44b0cfc68d27b91c6f127e17724d397a180
piedrahitapablo <piedra@meltstudio.co>
2023-05-25 08:30:30 -0500

    add gx remap to open links in browser

--- lua/piedra/remap.lua
+++ lua/piedra/remap.lua
@@ -64,3 +64,9 @@ vim.keymap.set(
     "<leader>sc",
     "y:,$s/<C-r>\"/<C-r>\"/gIc|1,''-&&<Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left>"
 )
+
+-- since netrw is disabled, get the gx functionality back
+-- https://stackoverflow.com/a/63352904/8133868
+-- if using other so, open needs to be changed
+-- FIXME: probably needs to be improved, it doesn't work in some cases
+vim.keymap.set("n", "gx", ":silent !open <C-r><C-a><CR>")
