ce3bd74d23a6093e3ceb20193e6d5606fe587024
piedrahitapablo <piedra@meltstudio.co>
2023-11-27 09:51:52 -0500

    add remap for console.log in ts and js

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,3 +1,6 @@
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+
+vim.keymap.set("n", "<leader>lg", "yiwoconsole.log('<Esc>pa', <Esc>pa);<Esc>")
+vim.keymap.set("v", "<leader>lg", "yoconsole.log('<Esc>pa', <Esc>pa);<Esc>")
