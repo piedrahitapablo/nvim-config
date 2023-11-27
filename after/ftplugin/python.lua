25c6dd4f925d6d2d6831abe6038b8701a67ef28e
piedrahitapablo <piedra@meltstudio.co>
2023-11-27 10:00:42 -0500

    add remap for print in python

---
+++ after/ftplugin/python.lua
@@ -0,0 +1,6 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.keymap.set("n", "<leader>lg", 'yiwoprint("<Esc>pa", <Esc>pa)<Esc>')
+vim.keymap.set("v", "<leader>lg", 'yoprint("<Esc>pa", <Esc>pa)<Esc>')
