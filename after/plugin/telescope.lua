169bf626ac4a24b1e750435488b480154a091e04
piedrahitapablo <piedra@meltstudio.co>
2023-11-27 09:57:26 -0500

    add telescope remaps for quickfix and grep_string

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -51,10 +51,14 @@ telescope.setup({
 
 vim.keymap.set("n", "<leader>tf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>ts", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>ty", function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
 vim.keymap.set("n", "<leader>tb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>td", builtin.diagnostics, {})
 vim.keymap.set("n", "<leader>tc", builtin.commands, {})
 vim.keymap.set("n", "<leader>tr", builtin.resume, {})
+vim.keymap.set("n", "<leader>tq", builtin.quickfix, {})
 
 vim.keymap.set("n", "<leader>tgf", builtin.git_files, {})
 vim.keymap.set("n", "<leader>tgc", builtin.git_commits, {})
