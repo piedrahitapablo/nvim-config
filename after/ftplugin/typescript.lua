58067438fbd5ca9b4eb198a2902cc974e1980aad
piedrahitapablo <piedra@meltstudio.co>
2023-12-11 14:27:34 -0500

    add global Prettier function for md, ts and js

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -4,3 +4,13 @@ vim.opt.shiftwidth = 2
 
 vim.keymap.set("n", "<leader>lg", "yiwoconsole.log('<Esc>pa', <Esc>pa);<Esc>")
 vim.keymap.set("v", "<leader>lg", "yoconsole.log('<Esc>pa', <Esc>pa);<Esc>")
+
+function Prettier()
+    local FilePath = vim.api.nvim_buf_get_name(0)
+
+    vim.fn.system({ "yarn", "prettier", "--write", FilePath })
+    vim.cmd("e!")
+end
+
+-- TODO: find a way to make this work
+-- vim.api.nvim_create_user_command("Prettier", "lua Prettier")
