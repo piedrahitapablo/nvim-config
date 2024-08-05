e67c436bf029d9302c0354f16550174fa2d6dd76
piedrahitapablo <juan.p@snappr.com>
2024-08-05 14:27:26 -0500

    add some fugitive commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -11,4 +11,55 @@ local function FugitiveStatus()
     end
 end
 
+local function GitBranchName()
+    local branch =
+        vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+    if branch ~= "" then
+        return branch
+    else
+        return nil
+    end
+end
+
 vim.keymap.set("n", "<leader>gs", FugitiveStatus)
+
+vim.api.nvim_create_user_command("Gfa", "G fetch --all --prune --jobs=10", {
+    desc = "Git fetch all",
+    force = false,
+})
+vim.api.nvim_create_user_command("Gp", function(opts)
+    vim.cmd(string.format("G push %s", opts.fargs[1]))
+end, {
+    desc = "Git push to upstream",
+    force = false,
+    nargs = 1,
+})
+vim.api.nvim_create_user_command("Gpsup", function()
+    local branch = GitBranchName()
+    if branch == nil or branch == "" then
+        print("Could not get the name of the current branch")
+    end
+
+    vim.cmd(string.format("G push --set-upstream origin %s", branch))
+end, {
+    desc = "Git push to upstream",
+    force = false,
+})
+vim.api.nvim_create_user_command("Grreb", function(opts)
+    vim.cmd.Gfa()
+    vim.cmd(string.format("G rebase -i origin/%s", opts.fargs[1]))
+end, {
+    desc = "Git rebase remote branch",
+    force = false,
+    nargs = 1,
+    complete = function()
+        return { "develop" }
+    end,
+})
+vim.api.nvim_create_user_command("Gc", function(opts)
+    vim.cmd(string.format("G commit %s", opts.fargs[1]))
+end, {
+    desc = "Git commit",
+    force = false,
+    nargs = 1,
+})
