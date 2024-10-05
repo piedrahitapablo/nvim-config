425b6b4f2327400c26815410cf03077de9f18aca
piedrahitapablo <juan.p@snappr.com>
2024-10-05 15:37:25 -0500

    update oil

--- lua/piedra/lazy/oil.lua
+++ lua/piedra/lazy/oil.lua
@@ -2,48 +2,52 @@ return {
     {
         "stevearc/oil.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            local oil = require("oil")
-            oil.setup({
-                delete_to_trash = true,
-                columns = {
-                    "icon",
-                    "permissions",
-                    -- "size",
-                    -- "mtime",
-                },
-                view_options = {
-                    -- Show files and directories that start with "."
-                    show_hidden = true,
-                    -- This function defines what is considered a "hidden" file
-                    is_hidden_file = function(name)
-                        return vim.startswith(name, ".")
-                    end,
-                    -- This function defines what will never be shown, even when `show_hidden` is set
-                    is_always_hidden = function()
-                        return false
-                    end,
-                },
-                float = {
-                    -- Padding around the floating window
-                    padding = 10,
-                    max_width = 0,
-                    max_height = 0,
-                    border = "rounded",
-                    win_options = {
-                        winblend = 10,
-                    },
-                },
-            })
-
-            -- TODO: improve this keymap to work well with ssh
-            vim.keymap.set(
-                "n",
+        keys = {
+            {
                 "<leader>pb",
-                string.format(":Oil %s<CR>", vim.fn.getcwd())
-            )
-            vim.keymap.set("n", "<leader>pv", ":Oil<CR>")
-            vim.keymap.set("n", "<leader>vpv", ":vertical Oil<CR>")
-        end,
+                function()
+                    vim.cmd.Oil(vim.fn.getcwd())
+                end,
+            },
+            { "<leader>pv", ":Oil<CR>" },
+            { "<leader>vpv", ":vertical Oil<CR>" },
+            { "<leader>fpv", ":Oil --float<CR>" },
+        },
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {
+            delete_to_trash = true,
+            columns = {
+                "icon",
+                "permissions",
+                -- "size",
+                -- "mtime",
+            },
+            view_options = {
+                -- Show files and directories that start with "."
+                show_hidden = true,
+                -- This function defines what is considered a "hidden" file
+                is_hidden_file = function(name)
+                    return vim.startswith(name, ".")
+                end,
+                -- This function defines what will never be shown, even when `show_hidden` is set
+                is_always_hidden = function()
+                    return false
+                end,
+            },
+            float = {
+                -- Padding around the floating window
+                padding = 10,
+                max_width = 0,
+                max_height = 0,
+                border = "rounded",
+                win_options = {
+                    winblend = 10,
+                },
+            },
+        },
+        lsp_file_methods = {
+            autosave_changes = "unmodified",
+        },
     },
 }
