e738d852da23276d689bacd312ea5e474961d347
piedrahitapablo <juan.p@snappr.com>
2024-10-05 13:00:02 -0500

    update treesitter plugins

--- lua/piedra/lazy/treesitter.lua
+++ lua/piedra/lazy/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
         config = function()
             local treesitter = require("nvim-treesitter.configs")
             treesitter.setup({
@@ -44,18 +45,21 @@ return {
     { "nvim-treesitter/playground" },
     {
         "nvim-treesitter/nvim-treesitter-context",
-        opts = {
-            enable = false,
-            max_lines = 0,
-            min_window_height = 0,
-            line_numbers = true,
-            multiline_threshold = 20,
-            trim_scope = "outer",
-            mode = "cursor",
-            separator = nil,
-            zindex = 20,
-        },
+        -- did not work using keys and opts
         config = function()
+            local treesitter_context = require("treesitter-context")
+            treesitter_context.setup({
+                enable = false,
+                max_lines = 0,
+                min_window_height = 0,
+                line_numbers = true,
+                multiline_threshold = 20,
+                trim_scope = "outer",
+                mode = "cursor",
+                separator = nil,
+                zindex = 20,
+            })
+
             vim.keymap.set("n", "<leader>kts", "<cmd>TSContextToggle<cr>")
         end,
     },
