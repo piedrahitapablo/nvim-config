312b437d39a9420f359e3189f98531d46b4b2cb3
piedrahitapablo <juan.p@snappr.com>
2024-10-06 13:55:58 -0500

    use keys for cellullar automaton

--- lua/piedra/lazy/trash.lua
+++ lua/piedra/lazy/trash.lua
@@ -1,11 +1,16 @@
 return {
     {
         "eandrju/cellular-automaton.nvim",
-        config = function()
-            vim.keymap.set("n", "<leader>fml", function()
-                local animations = { "make_it_rain", "game_of_life" }
-                vim.cmd.CellularAutomaton(animations[math.random(#animations)])
-            end)
-        end,
+        keys = {
+            {
+                "<leader>fml",
+                function()
+                    local animations = { "make_it_rain", "game_of_life" }
+                    vim.cmd.CellularAutomaton(
+                        animations[math.random(#animations)]
+                    )
+                end,
+            },
+        },
     },
 }
