d4905a20d997ce6f266d35117700618a2b9fb698
piedrahitapablo <juan.p@snappr.com>
2025-02-10 09:15:33 -0500

    make conform format markdown files

--- lua/piedra/lazy/conform.lua
+++ lua/piedra/lazy/conform.lua
@@ -42,6 +42,7 @@ return {
                 javascriptreact = { "prettier", stop_after_first = true },
                 typescript = { "prettier", stop_after_first = true },
                 typescriptreact = { "prettier", stop_after_first = true },
+                markdown = { "prettier", stop_after_first = true },
             },
             default_format_opts = {
                 lsp_format = "fallback",
