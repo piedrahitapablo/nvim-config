105573369601420059c7060bad363b85deb21f39
piedrahitapablo <juan.p@snappr.com>
2025-02-10 09:15:41 -0500

    fix gitlinker

--- lua/piedra/lazy/git.lua
+++ lua/piedra/lazy/git.lua
@@ -106,9 +106,23 @@ return {
         end,
     },
     {
-        "ruifm/gitlinker.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        "linrongbin16/gitlinker.nvim",
+        cmd = "GitLink",
         opts = {},
+        keys = {
+            {
+                "<leader>gy",
+                "<cmd>GitLink<cr>",
+                mode = { "n", "v" },
+                desc = "Yank git link",
+            },
+            {
+                "<leader>gY",
+                "<cmd>GitLink!<cr>",
+                mode = { "n", "v" },
+                desc = "Open git link",
+            },
+        },
     },
     {
         "lewis6991/gitsigns.nvim",
