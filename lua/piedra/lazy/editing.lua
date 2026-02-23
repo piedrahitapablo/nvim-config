e61e5ac87c498f0431ccffb2314a041299726576
piedrahitapablo <jpiedrahitaq@gmail.com>
2026-02-23 09:10:16 -0500

    add md renderer

--- lua/piedra/lazy/editing.lua
+++ lua/piedra/lazy/editing.lua
@@ -1,4 +1,11 @@
 return {
     { "windwp/nvim-ts-autotag" },
     { "tpope/vim-surround" },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+            file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+    },
 }
